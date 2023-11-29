66390c3fb21961b7230e932bdedcee234f74496d
davay <davay@defucc.me>
2023-11-29 15:15:42 +0700

    deploy

--- components/program/ProgramList.vue
+++ components/program/ProgramList.vue
@@ -7,7 +7,7 @@ const props = defineProps({
 </script>
 
 <template lang="pug">
-.flex.flex-wrap.max-w-55ch.gap-12
+.flex.flex-wrap.max-w-55ch.gap-12.sm-mx-4
   slot
   ProgramCard(v-for="program in programs", :key="program" v-bind="program")
 
